baf58639b8f8806f151031f3b36dd4576d5413ca
cyber-snowflake <72914371+cyber-snowflake@users.noreply.github.com>
2020-10-24 17:21:50 +0400

    Got rid of flags (for now) because it's serverless

--- components/Player/subcomponents/General.tsx
+++ components/Player/subcomponents/General.tsx
@@ -2,7 +2,6 @@ import { Fragment, FunctionComponent } from "react";
 import { Grid, Typography, makeStyles, Avatar } from "@material-ui/core";
 import { IModifiedUser } from "../../../interfaces";
 import { IGuildSimple } from "vime-types/models/Guild";
-import Badges from "../../Badges";
 
 type Props = {
   user: IModifiedUser;
@@ -30,24 +29,15 @@ const General: FunctionComponent<Props> = ({ user, guild }) => {
       <Fragment>
         <Grid container spacing={1}>
           <Grid item>
-            <Avatar
-              variant="rounded"
-              src={user.skinHelm}
-              className={classes.avatar}
-            />
+            <Avatar variant="rounded" src={user.skinHelm} className={classes.avatar} />
           </Grid>
 
           <Grid item xs sm>
             <Typography variant="h5" className={classes.username}>
-              {user.username}{" "}
-              {user.flags.raw > 0 && <Badges flags={user.flags.processed} />}
+              {user.username}
             </Typography>
-            <Typography
-              variant="h6"
-              className={classes.rank}
-              style={{ color: user.rankColor }}
-            >
-              {user.humanizedRank.toUpperCase()}
+            <Typography variant="h6" className={classes.rank} style={{ color: user.rankColor }}>
+              {user?.humanizedRank?.toUpperCase()}
             </Typography>
           </Grid>
         </Grid>
