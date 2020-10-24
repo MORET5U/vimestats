5ae6b05f80d2d05ec4c9240880b22054089a6c44
cyber-snowflake <72914371+cyber-snowflake@users.noreply.github.com>
2020-10-24 16:10:49 +0400

    Player rendering component

---
+++ components/Player/PlayerRenderer.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { UserData } from "../../interfaces";
+import { Grid, Container, Paper, Box } from "@material-ui/core";
+import Layout from "../Layout";
+import General from "./subcomponents/General";
+import Times from "./subcomponents/Times";
+import Leveling from "./subcomponents/Leveling";
+import Friends from "./subcomponents/Friends";
+import Stats from "./subcomponents/Stats";
+import Skin from "./subcomponents/Skin";
+
+type Props = {
+  data: UserData;
+};
+
+const PlayerRenderer: React.FunctionComponent<Props> = ({ data }) => {
+  const { user, session, friends, guild, stats } = data;
+
+  return (
+    <Layout
+      title={"[" + user?.humanizedRank + "] " + user?.username + " | VimeStats"}
+      description={
+        "Статистика игрока VimeWorld - " +
+        user?.username +
+        " | Уровень: " +
+        user?.level
+      }
+      color={user?.rankColor || "transparent"}
+      iconURL={user?.skinHelm}
+    >
+      <Container maxWidth="lg">
+        <Box>
+          {user && (
+            <Grid container spacing={2}>
+              <Grid item md={4} xs={12} sm={12}>
+                <Grid container spacing={1} direction="column">
+                  <Grid item md xs={12} sm={12}>
+                    <Paper className="gridItem" variant="outlined">
+                      <General user={user} guild={guild!} />
+                    </Paper>
+                  </Grid>
+
+                  <Grid item md xs={12} sm={12}>
+                    <Paper className="gridItem" variant="outlined">
+                      <Times session={session} user={user} />
+                    </Paper>
+                  </Grid>
+
+                  <Grid item md xs={12} sm={12}>
+                    <Paper className="gridItem" variant="outlined">
+                      <Leveling user={user} />
+                    </Paper>
+                  </Grid>
+
+                  <Grid item md xs={12} sm={12}>
+                    <Paper className="gridItem" variant="outlined">
+                      <Skin username={user.username} />
+                    </Paper>
+                  </Grid>
+
+                  <Grid item md xs={12} sm={12}>
+                    <Friends user={user} friends={friends} />
+                  </Grid>
+                </Grid>
+              </Grid>
+
+              <Grid item md={8} xs={12} sm={12}>
+                <Grid container direction="column">
+                  <Grid item>
+                    <Stats stats={stats!} />
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+          )}
+        </Box>
+      </Container>
+    </Layout>
+  );
+};
+
+export default PlayerRenderer;
