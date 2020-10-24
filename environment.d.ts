1b283a05c445202b8e8fda1a23b4ccb77c6f57fc
cyber-snowflake <72914371+cyber-snowflake@users.noreply.github.com>
2020-10-24 18:46:55 +0400

    Declared VK API key in env variables

--- environment.d.ts
+++ environment.d.ts
@@ -2,6 +2,7 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     VIME_API_KEY: string;
     VIME_API_URI: string;
+    VK_API_KEY: string;
     NEXT_PUBLIC_WEBSITE_BASE_URL: string;
   }
 }
