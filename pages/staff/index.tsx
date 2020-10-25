e5b6c4674eef62237b48c18d5f1d43411ff91e2a
cyber-snowflake <72914371+cyber-snowflake@users.noreply.github.com>
2020-10-25 16:46:07 +0400

    Converted staff page into functional component

--- pages/staff/index.tsx
+++ pages/staff/index.tsx
@@ -1,48 +1,44 @@
-import { Component } from "react";
 import Layout from "../../components/Layout";
 import { IOnlineModer } from "../../interfaces";
 import Axios, { AxiosError } from "axios";
 import OnlineStaffRenderer from "../../components/OnlineStaff/OnlineStaffRenderer";
+import { NextPage } from "next";
 
 type Props = {
   data?: IOnlineModer[];
   errors?: any;
 };
 
-class StaffPage extends Component<Props> {
-  public static getInitialProps = async () => {
-    try {
-      const req = await Axios.get(`${process.env.NEXT_PUBLIC_WEBSITE_BASE_URL}/api/online?sort=staff`);
-      const data = await req.data;
-
-      return { data };
-    } catch (e) {
-      let err: AxiosError = e;
-      return {
-        errors: { message: err.message, code: err.response?.status },
-      };
-    }
-  };
-
-  public render() {
-    const { data, errors } = this.props;
-
-    if (errors) {
-      return (
-        <Layout title={`Error`}>
-          <p>
-            <span style={{ color: "red" }}>Error:</span> <span>{errors.message}</span>
-          </p>
-        </Layout>
-      );
-    }
-
+const StaffPage: NextPage<Props> = ({ data, errors }) => {
+  if (errors) {
     return (
-      <Layout title="Онлайн модеры | VimeStats" description="Список онлайн модераторов на VimeWorld MiniGames">
-        {data && <OnlineStaffRenderer data={data} />}
+      <Layout title={`Error`}>
+        <p>
+          <span style={{ color: "red" }}>Error:</span> <span>{errors.message}</span>
+        </p>
       </Layout>
     );
   }
-}
+
+  return (
+    <Layout title="Онлайн модеры | VimeStats" description="Список онлайн модераторов на VimeWorld MiniGames">
+      {data && <OnlineStaffRenderer data={data} />}
+    </Layout>
+  );
+};
+
+StaffPage.getInitialProps = async () => {
+  try {
+    const req = await Axios.get(`${process.env.NEXT_PUBLIC_WEBSITE_BASE_URL}/api/online?sort=staff`);
+    const data = await req.data;
+
+    return { data };
+  } catch (e) {
+    let err: AxiosError = e;
+    return {
+      errors: { message: err.message, code: err.response?.status },
+    };
+  }
+};
 
 export default StaffPage;
