456e11a849ddf061b52c44b1fd537eceec892d7d
cyber-snowflake <72914371+cyber-snowflake@users.noreply.github.com>
2020-10-26 16:25:42 +0400

    Adjuced font sizes for accordions headings

--- components/Player/subcomponents/StatsAccordion.tsx
+++ components/Player/subcomponents/StatsAccordion.tsx
@@ -12,13 +12,13 @@ const useStyles = makeStyles((theme) => ({
     width: "100%",
   },
   heading: {
-    fontSize: theme.typography.pxToRem(15),
+    fontSize: theme.typography.pxToRem(16),
     flexBasis: "33.33%",
     flexShrink: 0,
     fontWeight: "bold",
   },
   secondaryHeading: {
-    fontSize: theme.typography.pxToRem(15),
+    fontSize: theme.typography.pxToRem(16),
     color: theme.palette.text.secondary,
   },
 }));
