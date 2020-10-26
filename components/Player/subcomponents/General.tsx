88856ba5743cc4fa555428d3b277708fc9cc1ea6
cyber-snowflake <72914371+cyber-snowflake@users.noreply.github.com>
2020-10-26 18:25:49 +0400

    Badges are finally back!

--- components/Player/subcomponents/General.tsx
+++ components/Player/subcomponents/General.tsx
@@ -2,6 +2,7 @@ import { Fragment, FunctionComponent } from "react";
 import { Grid, Typography, makeStyles, Avatar } from "@material-ui/core";
 import { IModifiedUser } from "../../../interfaces";
 import { IGuildSimple } from "vime-types/models/Guild";
+import Badges from "components/Badges";
 
 type Props = {
   user: IModifiedUser;
@@ -34,7 +35,7 @@ const General: FunctionComponent<Props> = ({ user, guild }) => {
 
           <Grid item xs sm>
             <Typography variant="h5" className={classes.username}>
-              {user.username}
+              {user.username} <Badges flags={user.flags!} />
             </Typography>
             <Typography variant="h6" className={classes.rank} style={{ color: user.rankColor }}>
               {user?.humanizedRank?.toUpperCase()}
