f4854d7ecf0a8c34ba0f27abf524e4c5a901d660
defracted <72914371+defracted@users.noreply.github.com>
2020-12-10 12:28:13 +0400

    PrgoressBar from now will be using different colors for different color modes

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -1,16 +1,16 @@
+import { useColorModeValue } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import NProgress from "nprogress";
 import { FC, useEffect } from "react";
 
 type Props = {
-  color?: string;
   startPosition?: number;
   stopDelayMs?: number;
   height?: number;
   options?: any;
 };
 
-const ProgressBar: FC<Props> = ({ color = "#29D", startPosition = 0.3, stopDelayMs = 200, height = 3, options }) => {
+const ProgressBar: FC<Props> = ({ startPosition = 0.3, stopDelayMs = 200, height = 3, options }) => {
   const router = useRouter();
 
   let timer: any = null;
@@ -28,6 +28,8 @@ const ProgressBar: FC<Props> = ({ color = "#29D", startPosition = 0.3, stopDelay
     }, stopDelayMs);
   };
 
+  const progressBarColor = useColorModeValue("#000000", "#AD87F3");
+
   useEffect(() => {
     if (options) {
       NProgress.configure(options);
@@ -44,7 +46,7 @@ const ProgressBar: FC<Props> = ({ color = "#29D", startPosition = 0.3, stopDelay
         pointer-events: none;
       }
       #nprogress .bar {
-        background: ${color};
+        background: ${progressBarColor};
         position: fixed;
         z-index: 5000;
         top: 0;
@@ -58,7 +60,7 @@ const ProgressBar: FC<Props> = ({ color = "#29D", startPosition = 0.3, stopDelay
         right: 0px;
         width: 100px;
         height: 100%;
-        box-shadow: 0 0 10px ${color}, 0 0 5px ${color};
+        box-shadow: 0 0 10px ${progressBarColor}, 0 0 5px ${progressBarColor};
         opacity: 1;
         -webkit-transform: rotate(3deg) translate(0px, -4px);
         -ms-transform: rotate(3deg) translate(0px, -4px);
@@ -76,8 +78,8 @@ const ProgressBar: FC<Props> = ({ color = "#29D", startPosition = 0.3, stopDelay
         height: 42px;
         box-sizing: border-box;
         border: solid 5px transparent;
-        border-top-color: ${color};
-        border-left-color: ${color};
+        border-top-color: ${progressBarColor};
+        border-left-color: ${progressBarColor};
         border-radius: 50%;
         -webkit-animation: nprogresss-spinner 400ms linear infinite;
         animation: nprogress-spinner 400ms linear infinite;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,12 @@ import "../styles/badges.scss";
 import "../styles/darkscroller.scss";
 import "../styles/demotions.scss";
 import "../styles/player.scss";
-
 import "focus-visible/dist/focus-visible";
-
 import { AppProps } from "next/app";
-import { FC, Fragment, useEffect, useState } from "react";
-
-import { ChakraProvider } from "@chakra-ui/react";
+import { FC, Fragment } from "react";
+import { ChakraProvider, CSSReset } from "@chakra-ui/react";
 import { Global, css } from "@emotion/react";
-
 import ProgressBar from "../components/ProgressBar";
-import ThemeCtx, { darkTheme } from "../components/Theme";
 
 const FocusOutlineHidden = css`
   .js-focus-visible :focus:not([data-focus-visible-added]) {
@@ -23,48 +18,14 @@ const FocusOutlineHidden = css`
 `;
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
-  const [themeState, setTheme] = useState({ isDark: false });
-
-  const setDark = () => {
-    localStorage.setItem("color-theme", "dark");
-    setTheme({ isDark: true });
-  };
-
-  const setLight = () => {
-    localStorage.setItem("color-theme", "light");
-    setTheme({ isDark: false });
-  };
-
-  const switchTheme = () => {
-    if (localStorage.getItem("color-theme")?.toLowerCase() === "dark") setLight();
-    else setDark();
-  };
-
-  useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector("#jss-server-side");
-    if (jssStyles) {
-      jssStyles.parentElement!.removeChild(jssStyles);
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.getItem("color-theme") === "dark" ? setDark() : setLight();
-  }, []);
-
   return (
     <Fragment>
-      <ThemeCtx.Provider value={{ themeState, setDark, setLight, switchTheme }}>
-        <ChakraProvider>
-          <Global styles={FocusOutlineHidden} />
-          <ProgressBar
-            height={6}
-            options={{ showSpinner: true }}
-            color={themeState.isDark ? darkTheme.palette.primary.main : "#000000"}
-          />
-          <Component {...pageProps} />
-        </ChakraProvider>
-      </ThemeCtx.Provider>
+      <ChakraProvider>
+        <CSSReset />
+        <Global styles={FocusOutlineHidden} />
+        <ProgressBar height={6} options={{ showSpinner: true }} />
+        <Component {...pageProps} />
+      </ChakraProvider>
     </Fragment>
   );
 };
