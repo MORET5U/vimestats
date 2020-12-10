df647baa85a7e8bc833ef7527e0fa3e346e913a1
defracted <72914371+defracted@users.noreply.github.com>
2020-12-10 12:31:57 +0400

    Created a custom theme for ChakraProvider

---
+++ components/customTheme.ts
@@ -0,0 +1,20 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const customTheme = extendTheme({
+  colors: {
+    brand: {
+      50: "#f0e7ff",
+      100: "#cfb9fa",
+      200: "#b08bf3",
+      300: "#905dee",
+      400: "#702fe8",
+      500: "#5717cf",
+      600: "#4411a1",
+      700: "#300c74",
+      800: "#1d0648",
+      900: "#0c011c",
+    },
+  },
+});
+
+export default customTheme;
