848b70bf0b398b4ba903531626a60378d6a96fc3
defracted <72914371+defracted@users.noreply.github.com>
2020-12-10 11:55:42 +0400

    ProgressBar is now also a functional component

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -1,123 +1,113 @@
-import Router from "next/router";
+import { useRouter } from "next/router";
 import NProgress from "nprogress";
-import { Component } from "react";
+import { FC, useEffect } from "react";
 
 type Props = {
   color?: string;
-  startPosition: number;
-  stopDelayMs: number;
-  height: number;
+  startPosition?: number;
+  stopDelayMs?: number;
+  height?: number;
   options?: any;
 };
 
-class ProgressBar extends Component<Props> {
-  public static defaultProps = {
-    color: "#29D",
-    startPosition: 0.3,
-    stopDelayMs: 200,
-    height: 3,
-  };
+const ProgressBar: FC<Props> = ({ color = "#29D", startPosition = 0.3, stopDelayMs = 200, height = 3, options }) => {
+  const router = useRouter();
 
-  public timer: any = null;
+  let timer: any = null;
 
-  public routeChangeStart = () => {
-    NProgress.set(this.props.startPosition);
+  const routeChangeStart = () => {
+    NProgress.set(startPosition);
     NProgress.start();
   };
 
-  public routeChangeEnd = () => {
-    clearTimeout(this.timer);
-    this.timer = setTimeout(() => {
+  const routeChangeEnd = () => {
+    clearTimeout(timer);
+
+    timer = setTimeout(() => {
       NProgress.done(true);
-    }, this.props.stopDelayMs);
+    }, stopDelayMs);
   };
 
-  public render() {
-    const { color, height } = this.props;
-
-    return (
-      <style jsx global>{`
-        #nprogress {
-          pointer-events: none;
-        }
-        #nprogress .bar {
-          background: ${color};
-          position: fixed;
-          z-index: 5000;
-          top: 0;
-          left: 0;
-          width: 100%;
-          height: ${height}px;
-        }
-        #nprogress .peg {
-          display: block;
-          position: absolute;
-          right: 0px;
-          width: 100px;
-          height: 100%;
-          box-shadow: 0 0 10px ${color}, 0 0 5px ${color};
-          opacity: 1;
-          -webkit-transform: rotate(3deg) translate(0px, -4px);
-          -ms-transform: rotate(3deg) translate(0px, -4px);
-          transform: rotate(3deg) translate(0px, -4px);
-        }
-        #nprogress .spinner {
-          display: "block";
-          position: fixed;
-          z-index: 5000;
-          bottom: 15px;
-          left: 15px;
-        }
-        #nprogress .spinner-icon {
-          width: 42px;
-          height: 42px;
-          box-sizing: border-box;
-          border: solid 5px transparent;
-          border-top-color: ${color};
-          border-left-color: ${color};
-          border-radius: 50%;
-          -webkit-animation: nprogresss-spinner 400ms linear infinite;
-          animation: nprogress-spinner 400ms linear infinite;
-        }
-        .nprogress-custom-parent {
-          overflow: hidden;
-          position: relative;
-        }
-        .nprogress-custom-parent #nprogress .spinner,
-        .nprogress-custom-parent #nprogress .bar {
-          position: absolute;
-        }
-        @-webkit-keyframes nprogress-spinner {
-          0% {
-            -webkit-transform: rotate(0deg);
-          }
-          100% {
-            -webkit-transform: rotate(360deg);
-          }
-        }
-        @keyframes nprogress-spinner {
-          0% {
-            transform: rotate(0deg);
-          }
-          100% {
-            transform: rotate(360deg);
-          }
-        }
-      `}</style>
-    );
-  }
-
-  public componentDidMount() {
-    const { options } = this.props;
-
+  useEffect(() => {
     if (options) {
       NProgress.configure(options);
     }
 
-    Router.events.on("routeChangeStart", this.routeChangeStart);
-    Router.events.on("routeChangeComplete", this.routeChangeEnd);
-    Router.events.on("routeChangeError", this.routeChangeEnd);
-  }
-}
+    router.events.on("routeChangeStart", routeChangeStart);
+    router.events.on("routeChangeComplete", routeChangeEnd);
+    router.events.on("routeChangeError", routeChangeEnd);
+  }, []);
+
+  return (
+    <style jsx global>{`
+      #nprogress {
+        pointer-events: none;
+      }
+      #nprogress .bar {
+        background: ${color};
+        position: fixed;
+        z-index: 5000;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: ${height}px;
+      }
+      #nprogress .peg {
+        display: block;
+        position: absolute;
+        right: 0px;
+        width: 100px;
+        height: 100%;
+        box-shadow: 0 0 10px ${color}, 0 0 5px ${color};
+        opacity: 1;
+        -webkit-transform: rotate(3deg) translate(0px, -4px);
+        -ms-transform: rotate(3deg) translate(0px, -4px);
+        transform: rotate(3deg) translate(0px, -4px);
+      }
+      #nprogress .spinner {
+        display: "block";
+        position: fixed;
+        z-index: 5000;
+        bottom: 15px;
+        left: 15px;
+      }
+      #nprogress .spinner-icon {
+        width: 42px;
+        height: 42px;
+        box-sizing: border-box;
+        border: solid 5px transparent;
+        border-top-color: ${color};
+        border-left-color: ${color};
+        border-radius: 50%;
+        -webkit-animation: nprogresss-spinner 400ms linear infinite;
+        animation: nprogress-spinner 400ms linear infinite;
+      }
+      .nprogress-custom-parent {
+        overflow: hidden;
+        position: relative;
+      }
+      .nprogress-custom-parent #nprogress .spinner,
+      .nprogress-custom-parent #nprogress .bar {
+        position: absolute;
+      }
+      @-webkit-keyframes nprogress-spinner {
+        0% {
+          -webkit-transform: rotate(0deg);
+        }
+        100% {
+          -webkit-transform: rotate(360deg);
+        }
+      }
+      @keyframes nprogress-spinner {
+        0% {
+          transform: rotate(0deg);
+        }
+        100% {
+          transform: rotate(360deg);
+        }
+      }
+    `}</style>
+  );
+};
 
 export default ProgressBar;
