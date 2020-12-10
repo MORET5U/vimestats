615eaf117136c91e730a7e57d7d6fd3b01a113cc
defracted <72914371+defracted@users.noreply.github.com>
2020-12-10 11:56:06 +0400

    Must be included according to chakra ui docs

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,18 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript, DocumentContext } from "next/document";
 import React from "react";
+import { ColorModeScript } from "@chakra-ui/react";
 
 export default class MyDocument extends Document {
+  static getInitialProps(ctx: DocumentContext) {
+    return Document.getInitialProps(ctx);
+  }
+
   render() {
     return (
       <Html lang="ru">
-        <Head>
-        </Head>
+        <Head></Head>
         <body>
+          <ColorModeScript />
           <Main />
           <NextScript />
         </body>
