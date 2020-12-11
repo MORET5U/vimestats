6c0bec7d25b188f9ea1cde044b3f7008ebff3718
defracted <72914371+defracted@users.noreply.github.com>
2020-12-11 20:55:58 +0400

    There's now an option to see freinds or stats via breadcrumbs

--- components/Player/PlayerBase.tsx
+++ components/Player/PlayerBase.tsx
@@ -1,6 +1,7 @@
-import { Box, Container, Grid, GridItem, VStack } from "@chakra-ui/react";
+import { Box, Breadcrumb, BreadcrumbItem, Button, Container, Grid, GridItem, VStack } from "@chakra-ui/react";
+import Paper from "components/Paper";
 import { isNil, merge, pick } from "lodash";
-import React from "react";
+import React, { useState } from "react";
 import { UserData } from "../../interfaces";
 import Layout from "../Layout";
 import PlayerLevel from "./PlayerLevel";
@@ -12,8 +13,12 @@ type Props = {
   data: UserData;
 };
 
+type SelectedTab = "stats" | "friends";
+
 const PlayerBase: React.FunctionComponent<Props> = ({ data }) => {
   const { user, session, friends, guild, stats, skin, cape } = data;
+
+  const [currentTab, setTab] = useState<SelectedTab>("stats");
 
   const playerSmallCard = merge(user, { guild });
   const playerTimeAndStatusProps = merge(session!, pick(user, ["playedSeconds", "lastSeen"]));
@@ -45,7 +50,26 @@ const PlayerBase: React.FunctionComponent<Props> = ({ data }) => {
               </GridItem>
 
               <GridItem>
-                <Box w="100%" h="120px" bg="green.200"></Box>
+                <VStack>
+                  <Paper>
+                    <Breadcrumb spacing="8px">
+                      <BreadcrumbItem>
+                        <Button onClick={() => setTab("stats")} isActive={currentTab === "stats"} size="sm">
+                          Статистика
+                        </Button>
+                      </BreadcrumbItem>
+
+                      <BreadcrumbItem>
+                        <Button onClick={() => setTab("friends")} isActive={currentTab === "friends"} size="sm">
+                          Друзья
+                        </Button>
+                      </BreadcrumbItem>
+                    </Breadcrumb>
+                  </Paper>
+
+                  {currentTab === "stats" && <Box w="100%" h="120px" bg="green.200"></Box>}
+                  {currentTab === "friends" && <Box w="100%" h="120px" bg="red.200"></Box>}
+                </VStack>
               </GridItem>
             </Grid>
           )}
