ffb2f1b87e50ec3b260144550c7698a2da1877f1
defracted <72914371+defracted@users.noreply.github.com>
2020-12-11 17:45:15 +0400

    base component for a player info page is now a little more filled with data

--- components/Player/PlayerBase.tsx
+++ components/Player/PlayerBase.tsx
@@ -1,7 +1,12 @@
-import { Box, Container, SimpleGrid } from "@chakra-ui/react";
+import { Box, Container, Grid, GridItem, VStack } from "@chakra-ui/react";
+import { isNil, merge, pick } from "lodash";
 import React from "react";
 import { UserData } from "../../interfaces";
 import Layout from "../Layout";
+import PlayerLevel from "./PlayerLevel";
+import PlayerSkin from "./PlayerSkin";
+import PlayerSmallCard from "./PlayerSmallCard";
+import PlayerTimeAndStatus from "./PlayerTimeAndStatus";
 
 type Props = {
   data: UserData;
@@ -10,6 +15,11 @@ type Props = {
 const PlayerBase: React.FunctionComponent<Props> = ({ data }) => {
   const { user, session, friends, guild, stats, skin, cape } = data;
 
+  const playerSmallCard = merge(user, { guild });
+  const playerTimeAndStatusProps = merge(session!, pick(user, ["playedSeconds", "lastSeen"]));
+  const playerLevelProps = pick(user, ["level", "levelPercentage"]);
+  const playerSkinProps = merge(pick(user, ["username"]), { skin, cape });
+
   return (
     <Layout
       title={"[" + user?.humanizedRank + "] " + user?.username + " | VimeStats"}
@@ -17,12 +27,27 @@ const PlayerBase: React.FunctionComponent<Props> = ({ data }) => {
       color={user?.rankColor || "transparent"}
       iconURL={user?.skinHelm3D}
     >
-      <Container maxW="4xl">
+      <Container maxW="6xl">
         <Box>
-          {user && (
-            <SimpleGrid>
-              
-            </SimpleGrid>
+          {!isNil(user) && (
+            <Grid
+              templateColumns={{ lg: "3fr 6fr", base: "1fr" }}
+              templateRows={{ lg: "1fr", base: "1fr 1fr 1fr 1fr 1fr 1fr" }}
+              gap={2}
+            >
+              <GridItem>
+                <VStack spacing={2}>
+                  <PlayerSmallCard {...playerSmallCard} />
+                  <PlayerTimeAndStatus {...playerTimeAndStatusProps} />
+                  <PlayerLevel {...playerLevelProps} />
+                  <PlayerSkin {...playerSkinProps} />
+                </VStack>
+              </GridItem>
+
+              <GridItem>
+                <Box w="100%" h="120px" bg="green.200"></Box>
+              </GridItem>
+            </Grid>
           )}
         </Box>
       </Container>
