d794c07fc0d0f2f83774e2400e91f241b2fff95e
defracted <72914371+defracted@users.noreply.github.com>
2020-12-17 23:17:06 +0400

    This font is no longer being used

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -20,7 +20,6 @@ const Layout: React.FunctionComponent<Props> = ({
   return (
     <Fragment>
       <GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" />
-      <GoogleFonts href="https://fonts.googleapis.com/css2?family=VT323&display=swap" />
 
       <Head>
         <meta charSet="utf-8" />
